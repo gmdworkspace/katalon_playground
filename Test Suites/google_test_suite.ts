7b8cd7558809866f780b300b99fc993190d9c21e
dinesh.gm <dinesh.gm@nationstarmail.com>
2018-07-28 18:05:23 +0530

    project commit

---
+++ Test Suites/google_test_suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>google_test_suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>51eae1f0-c6c8-4d49-be2c-80635a09c5b3</testSuiteGuid>
+</TestSuiteEntity>
